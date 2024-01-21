f97796ba85ddcb38b8088f014e163fc2a3c648d1
cd <lesleyrotonto@gmail.com>
2024-01-21 17:20:11 -0500

    remove unneeded comments

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,7 @@
 //will start without auth and once we get it working in the backen will add it in
 
 async function singup(user){
-    //this gets exported to the signup form
     try {
-        //will come back and change the route not created yet
         const res = await axios.post('http://localhost:3000/users', user)
         const json = res.data
         if (json.err) {
