ae8bf8a463b31ffff4e0adbc3b5e5418a93e044d
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-01-26 13:51:11 -0500

    add functionality to profile page

--- src/components/profile/ProfileComponent.jsx
+++ src/components/profile/ProfileComponent.jsx
@@ -15,9 +15,25 @@ const ProfileComponent = ( props ) => {
         fetchProfile()
     },[])
 
-    // define another variable to grab posts 
+    const [posts, setPosts] = useState([])
+    useEffect((arr)=>{
+        const fetchPosts = async (arr) =>{
+            const userPosts= await protectedServices.getMultiplePosts(arr)
+            console.log(userPosts)
+            setPosts(userPosts)
+        }
+        fetchPosts(arr)
+        
+    }, [])
+
+
+
+    
+
+    // use axios to grab the posts from each ID
 
     console.log(display, 'display')
+    // ['65b3f21b1cc3d7e50837925d', '65b3f2401cc3d7e508379266']
     return (
         <main className='container'>
             <h1>{props.user ? `Hello, ${props.user.username}` : 'Loading...'}</h1>
@@ -26,20 +42,20 @@ const ProfileComponent = ( props ) => {
             // mapping out the display state variable 
             <div key={`post${index}`} className="post-container">
             <Link to={`/viewpost/${post._id}`} key={post._id}>{post.title}</Link>
-            <h4  key={post.content}className="post-content">{post.content}</h4>
+            <h4 key={post.content}className="post-content">{post.content}</h4>
 
             <div className="choices-container">
                 <div className="choices1-container">
                 <div className="image-container">
-                <img key={`img$[0]`} src={post.img[0]} className="post-image"/>
+                {/* <img key={`img$[0]`} src={post.img[0]} className="post-image"/> */}
                 </div>
                 
                 <div className="choice-box">
-                    <button name="count1" className="choices choice1" key="choice1" 
+                    {/* <button name="count1" className="choices choice1" key="choice1" 
                     post={post} data-postid={post._id} onClick={(e) => onClick(e, post, "count1")}
                     disabled={clickedPosts[post._id]?.count1 || clickedPosts[post._id]?.count2 || post.count1.includes(thisUser.username) || post.count2.includes(thisUser.username) }>
                     {post.choice1}
-                    </button>
+                    </button> */}
                     <p  key='choice1-count'className="choice-count1">{post.count1.length}</p>
                 </div>
                 </div>
@@ -50,12 +66,12 @@ const ProfileComponent = ( props ) => {
                 </div>
                 
                 <div className="choice-box">
-                    <button className="choices choice2" key="choice2"
+                    {/* <button className="choices choice2" key="choice2"
                     post={post} data-postid={post._id}
                     onClick={(e) => onClick(e, post, "count2")}
                     disabled={clickedPosts[post._id]?.count1 || clickedPosts[post._id]?.count2 || post.count1.includes(thisUser.username) || post.count2.includes(thisUser.username) }>
                     {post.choice2}
-                    </button>
+                    </button> */}
                     <p key='choice2-count' className="choice-count2">{post.count2.length}</p>
                 </div>
                 </div>
