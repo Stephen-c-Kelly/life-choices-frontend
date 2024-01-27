17c915cdfcfb5fda601636f24990c669947f5cae
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-01-27 11:31:12 -0500

    troubleshooting

--- src/services/protectedServices.js
+++ src/services/protectedServices.js
@@ -2,8 +2,8 @@
 import * as tokenService from './tokenService'
 import axios from 'axios'
 
-// const baseUrl = `http://localhost:3000`
-const baseUrl = `https://lifechoices-a9061aaee4a7.herokuapp.com`
+const baseUrl = `http://localhost:3000`
+// const baseUrl = `https://lifechoices-a9061aaee4a7.herokuapp.com`
 
 async function getUserProfile(){
     try {
@@ -173,9 +173,7 @@ export{
     getMultiplePosts,
     addCommentToId,
     getCommentsfromPostId,
-    singleProfile,
-    updatePostChoice,
-    singleProfile
-
+    updatePostChoice
+   
 
 }
